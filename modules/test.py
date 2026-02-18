6862ce3553d29ba3c9c08bfded063b2593e6af3d
saaketh13 <saaketh.raghava@gmail.com>
2026-02-18 15:49:03 -0800

    Added the datasets to everything.

---
+++ modules/test.py
@@ -0,0 +1,5 @@
+import os
+
+print("CWD:", os.getcwd())
+print("Exists?", os.path.exists("/home/saaketh/CodingProjects/COGS108/hockey_data_analysis/Group118_WI26/data/00-raw/hart"))
+print("Files:", os.listdir("/home/saaketh/CodingProjects/COGS108/hockey_data_analysis/Group118_WI26/data/00-raw/hart"))
