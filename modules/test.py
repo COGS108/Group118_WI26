9e8982d08bf41599c5d7ceb485ec2ac4454e50ca
saaketh13 <saaketh.raghava@gmail.com>
2026-02-18 18:43:06 -0800

    Deleted the personal test files

--- modules/test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print("CWD:", os.getcwd())
-print("Exists?", os.path.exists("/home/saaketh/CodingProjects/COGS108/hockey_data_analysis/Group118_WI26/data/00-raw/hart"))
-print("Files:", os.listdir("/home/saaketh/CodingProjects/COGS108/hockey_data_analysis/Group118_WI26/data/00-raw/hart"))
